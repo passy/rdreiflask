7fea1388b392587e5d2277f58e9c913bcb154200
Pascal Hartig (Home) <phartig@rdrei.net>
2010-05-15 19:53:19 +0200

    Improved html output

--- rdrei/static/js/rdrei.js
+++ rdrei/static/js/rdrei.js
@@ -41,7 +41,7 @@ $.widget("rdrei.topMenu", {
 
         $.address.crawlable(this.options.crawlable);
         // Globally enabled.
-        $("a[href]:not([href^=http])").address();
+        $().address();
         // Save the initial color.
         this._init_colorchanger();
         $.address.internalChange(function (event) {
